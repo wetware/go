6db9d6f223a8a57f2ef84f13755b5722ed6105c1
Louis Thibault <l.thibault@sentimens.com>
2024-05-08 18:06:07 -0400

    Add -ipfs flag to ww run.

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -1,9 +1,13 @@
 package run
 
 import (
+	"net/http"
+
 	"github.com/ipfs/kubo/client/rpc"
+	iface "github.com/ipfs/kubo/core/coreiface"
 	"github.com/libp2p/go-libp2p"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	ma "github.com/multiformats/go-multiaddr"
 	"github.com/thejerf/suture/v4"
 	"github.com/urfave/cli/v2"
 	ww "github.com/wetware/go"
@@ -14,37 +18,59 @@ func Command() *cli.Command {
 	return &cli.Command{
 		Name: "run",
 		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "ipfs",
+				EnvVars: []string{"WW_IPFS"},
+				Usage:   "multi`addr` of IPFS node, or \"local\"",
+				Value:   "local",
+			},
 			&cli.StringSliceFlag{
 				Name:    "load",
 				EnvVars: []string{"WW_LOAD"},
 			},
 		},
-		Action: run,
+		Before: setup(),
+		Action: run(),
 	}
 }
 
-func run(c *cli.Context) error {
-	node, err := rpc.NewLocalApi()
-	if err != nil {
-		return err
+// Global IPFS node.  This is usually colocated on the local host,
+// or available on the local network.  By default, the setup func
+// will search for a local ~/.ipfs directory.
+var node iface.CoreAPI
+
+func setup() cli.BeforeFunc {
+	return func(c *cli.Context) (err error) {
+		var a ma.Multiaddr
+		if s := c.String("ipfs"); s == "local" {
+			node, err = rpc.NewLocalApi()
+		} else if a, err = ma.NewMultiaddr(s); err == nil {
+			node, err = rpc.NewApiWithClient(a, http.DefaultClient)
+		}
+
+		return
 	}
+}
 
-	h, err := libp2p.New()
-	if err != nil {
-		return err
-	}
+func run() cli.ActionFunc {
+	return func(c *cli.Context) error {
+		h, err := libp2p.New()
+		if err != nil {
+			return err
+		}
 
-	wetware := suture.New("ww", suture.Spec{
-		EventHook: util.EventHook,
-	})
-
-	for _, s := range c.StringSlice("load") {
-		wetware.Add(&ww.Cluster{
-			Name: s,
-			IPFS: node,
-			Host: routedhost.Wrap(h, node.Routing()),
+		wetware := suture.New("ww", suture.Spec{
+			EventHook: util.EventHook,
 		})
-	}
 
-	return wetware.Serve(c.Context)
+		for _, s := range c.StringSlice("load") {
+			wetware.Add(&ww.Cluster{
+				Name: s,
+				IPFS: node,
+				Host: routedhost.Wrap(h, node.Routing()),
+			})
+		}
+
+		return wetware.Serve(c.Context)
+	}
 }
