3db39154859db9e22e762b88bbec6db8a918b221
Louis Thibault <l.thibault@sentimens.com>
2024-11-15 21:35:43 -0500

    Add example/deliver.

--- examples/deliver/main.go
+++ examples/deliver/main.go
@@ -1,103 +1,47 @@
-// //go:generate tinygo build -o main.wasm -target=wasi -scheduler=none main.go
+//go:generate tinygo build -o main.wasm -target=wasi -scheduler=none main.go
 
 package main
 
-// import (
-// 	"bytes"
-// 	"context"
-// 	"io"
-// 	"log/slog"
-// 	"os"
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+)
 
-// 	"capnproto.org/go/capnp/v3"
-// 	"github.com/urfave/cli/v2"
-// 	"github.com/wetware/go/proc"
-// )
+func main() {
+	if len(os.Args) < 1 {
+		slog.Error("expected 1 argument, got 0",
+			"status", 1)
+		os.Exit(1)
+	}
 
-// func main() {
-// 	ctx := context.TODO()
+	f, err := os.Open(os.Args[0])
+	if err != nil {
+		slog.Error("failed to open file",
+			"reason", err,
+			"status", 2,
+			"name", os.Args[0])
+		os.Exit(2)
+	}
+	defer f.Close()
 
-// 	app := &cli.App{
-// 		Name:      "deliver",
-// 		Usage:     "read message from stdn and send to `PID`",
-// 		ArgsUsage: "<PID>",
-// 		Flags: []cli.Flag{
-// 			&cli.StringFlag{
-// 				Name:    "method",
-// 				Aliases: []string{"m"},
-// 				Usage:   "method name",
-// 			},
-// 			&cli.Uint64SliceFlag{
-// 				Name:    "push",
-// 				Aliases: []string{"p"},
-// 				Usage:   "push u64 onto stack",
-// 			},
-// 		},
-// 		Action: deliver,
-// 	}
+	var n int64
+	var status int
+	if n, err = io.Copy(f, os.Stdin); err != nil {
+		status = 3
+		err = fmt.Errorf("request: %w", err)
+	} else if n, err = io.Copy(os.Stdout, f); err != nil {
+		status = 4
+		err = fmt.Errorf("response: %w", err)
+	}
 
-// 	if err := app.RunContext(ctx, os.Args); err != nil {
-// 		slog.ErrorContext(ctx, "application failed",
-// 			"reason", err)
-// 		os.Exit(1)
-// 	}
-// }
+	if err != nil {
+		slog.Error("failed to read message from stdin",
+			"reason", err,
+			"status", status,
+			"read", n)
+	}
 
-// func deliver(c *cli.Context) error {
-// 	name := c.Args().First()
-// 	f, err := os.Open(name)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer f.Close()
-
-// 	m, seg := capnp.NewSingleSegmentMessage(nil)
-// 	defer m.Release()
-
-// 	call, err := proc.NewRootMethodCall(seg)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = call.SetName(c.String("method"))
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	stack := c.Uint64Slice("stack")
-// 	size := int32(len(stack))
-
-// 	callStack, err := call.NewStack(size)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	for i, word := range stack {
-// 		callStack.Set(i, word)
-// 	}
-
-// 	r := io.LimitReader(c.App.Reader, 1<<32-1) // max u32
-// 	data, err := io.ReadAll(r)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	if err = call.SetCallData(data); err != nil {
-// 		return err
-// 	}
-
-// 	b, err := m.Marshal()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	n, err := io.Copy(f, bytes.NewReader(b))
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	slog.DebugContext(c.Context, "delivered message",
-// 		"size", n,
-// 		"dest", name)
-// 	return nil
-// }
+	os.Exit(status)
+}
