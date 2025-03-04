71a6df47740c62440c7b3fafab89c42b2509531b
Louis Thibault <l.thibault@sentimens.com>
2025-03-04 18:06:27 -0500

    Clean up util.EventHook formatting.

--- util/service.go
+++ util/service.go
@@ -11,26 +11,36 @@ var EventHook = EventHookWithContext(context.Background())
 
 func EventHookWithContext(ctx context.Context) suture.EventHook {
 	return func(e suture.Event) {
-		var args []any
-		for k, v := range e.Map() {
-			args = append(args, k, v)
-		}
-
 		switch e.Type() {
 		case suture.EventTypeBackoff:
-			slog.InfoContext(ctx, e.String(), args...)
+			ev := e.(suture.EventBackoff)
+			slog.InfoContext(ctx, "entered backoff state",
+				"supervisor", ev.SupervisorName)
 
 		case suture.EventTypeResume:
-			slog.InfoContext(ctx, e.String(), args...)
+			ev := e.(suture.EventResume)
+			slog.InfoContext(ctx, "resumed",
+				"supervisor", ev.SupervisorName)
 
 		case suture.EventTypeServicePanic:
-			slog.WarnContext(ctx, e.String(), args...)
+			ev := e.(suture.EventServicePanic)
+			slog.ErrorContext(ctx, "service panicked",
+				"supervisor", ev.SupervisorName,
+				"service", ev.ServiceName,
+				"reason", ev.PanicMsg)
 
 		case suture.EventTypeServiceTerminate:
-			slog.WarnContext(ctx, e.String(), args...)
+			ev := e.(suture.EventServiceTerminate)
+			slog.WarnContext(ctx, "service terminated",
+				"supervisor", ev.SupervisorName,
+				"service", ev.ServiceName,
+				"reason", ev.Err)
 
 		case suture.EventTypeStopTimeout:
-			slog.ErrorContext(ctx, e.String(), args...)
+			ev := e.(suture.EventStopTimeout)
+			slog.ErrorContext(ctx, "service failed to stop",
+				"supervisor", ev.SupervisorName,
+				"service", ev.ServiceName)
 
 		default:
 			panic(e) // unhandled event
