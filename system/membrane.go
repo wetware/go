41e52a804cb001919a8ea56bc2d20e5858eeaed2
Louis Thibault <l.thibault@sentimens.com>
2025-08-28 16:52:06 -0400

    feat: implement membrane with service token support and export capability
    - Update Import method to use ServiceToken instead of Envelope
    - Add Export method implementation with random token generation
    - Improve service reference management with AddRef()

--- system/membrane.go
+++ system/membrane.go
@@ -2,6 +2,7 @@ package system
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 
 	capnp "capnproto.org/go/capnp/v3"
@@ -14,7 +15,7 @@ type ServiceToken [20]byte // 20 bytes = 160 bits = 20 hex characters
 type Membrane map[ServiceToken]capnp.Client
 
 func (m Membrane) Import(ctx context.Context, call Importer_import) error {
-	raw, err := call.Args().Envelope()
+	raw, err := call.Args().ServiceToken()
 	if err != nil {
 		return err
 	}
@@ -29,5 +30,23 @@ func (m Membrane) Import(ctx context.Context, call Importer_import) error {
 		return err
 	}
 
-	return res.SetService(m[token])
+	return res.SetService(m[token].AddRef())
+}
+
+func (m Membrane) Export(ctx context.Context, call Exporter_export) error {
+	var token ServiceToken
+	if n, err := rand.Read(token[:]); err != nil {
+		return err
+	} else if n != len(token) {
+		return fmt.Errorf("failed to read random bytes: got %d, want %d", n, len(token))
+	}
+
+	res, err := call.AllocResults()
+	if err != nil {
+		return err
+	}
+
+	service := call.Args().Service()
+	m[token] = service.AddRef()
+	return res.SetServiceToken(token[:])
 }
